1966ae080d8cb95ba52b735bab525f8eedc6dfbf
Carles Rigat Almansa <carlesrigat@gmail.com>
2022-04-02 10:12:45 +0200

    Initial commit

---
+++ web-app/src/main/java/cat/tecnocampus/elcercle/ElCercleApplication.java
@@ -0,0 +1,13 @@
+package cat.tecnocampus.elcercle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElCercleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElCercleApplication.class, args);
+    }
+
+}
---
+++ web-app/src/test/java/cat/tecnocampus/elcercle/ElCercleApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.tecnocampus.elcercle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElCercleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
