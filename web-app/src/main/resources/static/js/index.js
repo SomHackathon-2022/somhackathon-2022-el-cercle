e8e0946ec5efd8fa53107b1423be9dc9ae1954ab
Carles Rigat Almansa <carlesrigat@gmail.com>
2022-04-03 03:13:33 +0200

    Basic front end implementation. Text font to customise.
    Currently, the amount of kilograms of organic material from a restaurant are displayed, and a POST api rest call is able to modify its value, to be shown when the page is reloaded.

--- web-app/src/main/resources/static/js/index.js
+++ web-app/src/main/resources/static/js/index.js
@@ -1,24 +1,13 @@
 $(document).ready(update_values);
 
-function update_valuesB(){
-    alert("Inici");
-    $.getJSON('http://localhost:8080/organic', function(data) {
-        $('#organic_quantity_div').append
-        ('<h2>data[0]<\h2>');
-    });
-
-}
-
 
 function update_values(){
-    alert("comemnca");
     $.ajax({
         type: "GET",
         url: "http://localhost:8080/organic",
         dataType: "text"
     }).done(function (res) {
-        // Your `success` code
-        alert("yupiiiiii");
+        $('#organic_quantity_div').append('<h2>'+res+'<\h2>');
     }).fail(function (jqXHR, textStatus, errorThrown) {
         alert("AJAX call failed: " + textStatus + ", " + errorThrown);
     });
