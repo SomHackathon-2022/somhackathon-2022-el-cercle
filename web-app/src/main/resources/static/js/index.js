0d64bd970fde8c261881a51418b574f1f9f02e94
Carles Rigat Almansa <carlesrigat@gmail.com>
2022-04-03 02:59:03 +0200

    Tests, but still not working front end

--- web-app/src/main/resources/static/js/index.js
+++ web-app/src/main/resources/static/js/index.js
@@ -1,12 +1,27 @@
-var img_array = ['images/img_1_sol.png', 'images/img_2_sol_fertil.png', 'images/img_3_sol_conreat.png', 'images/img_4_restaurant_tancat.png', 'images/img_5_restaurant_obert.png'];
-var indexes = [0,0,0,0,     0,0,0,0,    0,0,0,0,    0,0,0,0]
+$(document).ready(update_values);
 
-function changeImg(index) {
-    var id = "btn" + index;
-    document.getElementById(id).src = img_array[indexes[index]];
-    indexes[index] ++;
-    if (indexes[index] == img_array.length - 1) {
-        indexes[index] = 0;
-    }
-    alert(img_array[indexes[index]]);
+function update_valuesB(){
+    alert("Inici");
+    $.getJSON('http://localhost:8080/organic', function(data) {
+        $('#organic_quantity_div').append
+        ('<h2>data[0]<\h2>');
+    });
+
 }
+
+
+function update_values(){
+    alert("comemnca");
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/organic",
+        dataType: "text"
+    }).done(function (res) {
+        // Your `success` code
+        alert("yupiiiiii");
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        alert("AJAX call failed: " + textStatus + ", " + errorThrown);
+    });
+}
+
+
